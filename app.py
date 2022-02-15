4c015779f3f9c608d64c3b213dc50693052d303e
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2022-02-15 15:39:43 +0100

    Try to ls directory

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+import os 
+os.system("ls -l")
+
 URL = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
 XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
 TIMEOUT = 20
