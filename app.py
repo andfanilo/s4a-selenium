05e07ff7432f6d058ae5635eababcfd877571f8b
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2022-02-15 15:50:58 +0100

    Use Service instead of executable_path

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -19,7 +20,7 @@ firefoxOptions = Options()
 firefoxOptions.add_argument("--headless")
 driver = webdriver.Firefox(
     options=firefoxOptions,
-    executable_path="/home/appuser/.conda/bin/geckodriver",
+    service=Service("/home/appuser/.conda/bin/geckodriver"),
 )
 driver.get(URL)
 
